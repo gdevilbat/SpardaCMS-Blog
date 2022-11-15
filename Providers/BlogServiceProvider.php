bcab378af4789c42af160011adc548fe5d84f573
maldinuribrahim <maldinuribrahim@gmail.com>
2022-11-15 18:22:38 +0700

    Laravel 8

--- Providers/BlogServiceProvider.php
+++ Providers/BlogServiceProvider.php
@@ -24,7 +24,7 @@ class BlogServiceProvider extends ServiceProvider
         $this->registerTranslations();
         $this->registerConfig();
         $this->registerViews();
-        $this->registerFactories();
+        //$this->registerFactories();
         $this->loadMigrationsFrom(__DIR__ . '/../Database/Migrations');
     }
 
